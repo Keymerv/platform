8f8256283b317cb2055c7bfffeec7e3ff19fd206
Keymerv <usextools@gmail.com>
2026-07-21 10:55:29 +0300

    Update supabase-config.js

--- public/assets/js/supabase-config.js
+++ public/assets/js/supabase-config.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // بيانات الاتصال الخاصة بمشروعك
 const SUPABASE_URL = 'https://nlbbgxazoiylneagusen.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_3088Sqrhuz6CTI0...'; // <-- استبدلها بالرمز الكامل من الشاشة
+const SUPABASE_KEY = 'sb_publishable_3088Sqrhuz6CTI06GqCiuA_GIlWtFXz'; // <-- استبدلها بالرمز الكامل من الشاشة
 
 // إنشاء عميل Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
